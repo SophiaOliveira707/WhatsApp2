a927e7e7e9ac99e02b7c1ade48b6a0eed41df38f
Sophia Oliveira <sophialaviniaoliveira@gmail.com>
2025-02-05 10:20:15 -0300

    comentarios e comandos git

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WhatsApp2
 WhatsApp 2 (Projeto de Daniel e Marçal)
+
+## Comandos GIT
+- Clonar repositório `git clone <link>`
+- Puxar alterações do github `git pull origin main`
+- Adicionar arquivos na fila de commit `git add .`
+- Salvar mudanças (commit) `git commit -m "<mensagem>"`
+- Lançar mudanças no github `git push origin main`
--- private/server.js
+++ private/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const server = express();
 const port = 80;
 
-app.use(express.static(path.join(__dirname,'..','public')));
+server.use(express.static(path.join(__dirname,'..','public')));// libera acesso a pasta public
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {//retorna a pagina inicial ao acessar rota raiz
   res.sendFile(path.join(__dirname,'..','public','home.html'));
-})
+});
 
-app.listen(port, () => {
+server.listen(port, () => {//servidor ouvindo na porta 80 (padrão do navegador)
   console.log(`Servidor rodando na porta ${port}`);
-})
+});
